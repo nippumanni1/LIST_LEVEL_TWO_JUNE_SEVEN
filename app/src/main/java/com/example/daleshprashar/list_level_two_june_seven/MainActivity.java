d0b69938059ca57d5f5824d7d4f42463cc39da1f
nippumanni1 <nippumanni1@gmail.com>
2017-06-11 01:17:04 -0500

    trying to understand new branch

---
+++ app/src/main/java/com/example/daleshprashar/list_level_two_june_seven/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.daleshprashar.list_level_two_june_seven;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+ListView listView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+      listView= (ListView)findViewById(R.id.);
+    }
+}
